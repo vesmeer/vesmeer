0ef3e6cf26b18220c57f4ab3132442f15c2c4f11
Petr Kout <kout.petr@gmail.com>
2015-03-29 21:38:29 -0400

    Added Fork me on GitHub. Made Saturn brighter.

--- Src/Config/fabfile_config_example.py
+++ Src/Config/fabfile_config_example.py
@@ -3,7 +3,7 @@ import os
 thisDir = os.path.dirname(os.path.abspath(__file__))
 
 SERVER_HOSTS = ['vesmeer.com']
-SERVER_USERNAME = 'petr'
+SERVER_USERNAME = 'myuser'
 SERVER_APP_ROOT_DIR = ''
 FORWARD_AGENT = True
 CONFIG_DIR = os.path.join(thisDir, '..', 'Config')
